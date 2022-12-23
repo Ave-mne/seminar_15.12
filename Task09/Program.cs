dbc47f32e951272c5f81418a6862ae698ab8d5b3
«Valeev_Andrey» <Valeevptk@yandex.ru>
2022-12-23 13:51:18 +0300

    Добавлены решения 9 задачи

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -10,7 +10,22 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}:");
 
-int firstDijit = number / 10;
-int secondDijit = number % 10;
-if(firstDijit > secondDijit) Console.WriteLine($"Наибольшая цифра числа => {firstDijit}");
-else Console.WriteLine($"Наибольшая цифра числа => {secondDijit}");
+// int firstDijit = number / 10;
+// int secondDijit = number % 10;
+// if(firstDijit > secondDijit) Console.WriteLine($"Наибольшая цифра числа => {firstDijit}");
+// else Console.WriteLine($"Наибольшая цифра числа => {secondDijit}");
+
+// int result = firstDijit > secondDijit ? firstDijit : secondDijit;
+// Console.WriteLine($"Наибольшая цифра числа => {result}");
+
+int maxDijit = MaxDijit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDijit}");
+
+
+int MaxDijit(int num)  // создание метода(функции)  // num = number
+{
+    int firstDijit = num / 10;
+    int secondDijit = num % 10;
+    // int result = firstDijit > secondDijit ? firstDijit : secondDijit;
+    return firstDijit > secondDijit ? firstDijit : secondDijit;;
+}
