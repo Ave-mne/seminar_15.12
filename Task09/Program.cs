fa3c40740b24c1b390dc061299b9815c33415124
«Valeev_Andrey» <Valeevptk@yandex.ru>
2022-12-23 14:17:23 +0300

    Решена 11 задача

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -14,13 +14,11 @@ Console.WriteLine($"Случайное число из отрезка 10 - 99 =>
 // int secondDijit = number % 10;
 // if(firstDijit > secondDijit) Console.WriteLine($"Наибольшая цифра числа => {firstDijit}");
 // else Console.WriteLine($"Наибольшая цифра числа => {secondDijit}");
-
 // int result = firstDijit > secondDijit ? firstDijit : secondDijit;
 // Console.WriteLine($"Наибольшая цифра числа => {result}");
 
 int maxDijit = MaxDijit(number);
 Console.WriteLine($"Наибольшая цифра числа => {maxDijit}");
-
 
 int MaxDijit(int num)  // создание метода(функции)  // num = number
 {
---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+int firstDijit = number / 100;
+int thirdDijit = number % 10;
+
+int result = firstDijit*10 + thirdDijit;
+Console.WriteLine($" Результат из {number} => {result}");
