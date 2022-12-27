1d83e64fad52fc3fc8039e01db6a08de1c601698
«Valeev_Andrey» <Valeevptk@yandex.ru>
2022-12-27 23:28:05 +0300

    Доделана 21 задача

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -23,6 +23,6 @@ Console.WriteLine($"Растояние между точками {xa}, {xb} и {
 
 double Distance(int x1, int x2, int y1 ,int y2, int z1, int z2)
 {
-    double distance = Math.Sqrt((Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2)+ + Math.Pow((z2-z1), 2))); 
+    double distance = Math.Sqrt((Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2))); 
     return distance;
 }
