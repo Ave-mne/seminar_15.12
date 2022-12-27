907eec45ca60200241ff16d4fca0df35ea037b2d
«Valeev_Andrey» <Valeevptk@yandex.ru>
2022-12-27 15:56:42 +0300

    Решена 18 задача

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая по задонному номеру четверти, 
+// показывает диапозон возможных координат точек в этой четверти(Х и У)
+
+Console.WriteLine("Введите номер четверти точки");
+
+string quarter = Console.ReadLine();
+
+string result = QuarterCoordinates(quarter);
+Console.WriteLine(result);
+
+string QuarterCoordinates(string number)
+{
+    if(number == "1") return "X > 0 и Y > 0";
+    if(number == "2") return "X < 0 и Y > 0";
+    if(number == "3") return "X < 0 и Y < 0";
+    if(number == "4") return "X > 0 и Y < 0";
+    return "Неверный номер четверти";
+}
