a4009f20e778614dd42c903c2d96cf3f2deadd97
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-01-13 14:29:35 +0300

    Решена 26 задача

---
+++ Task26/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдает количество цифр в числе.
+// 456 -> 3
+// 78 -> 2.
+// 89126 -> 5
+
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int countNumber = CountNumber(number);
+
+if (number == 0)
+{
+    Console.WriteLine($"Количество чисел в числе {number} равно 1");
+}
+else
+{
+    Console.WriteLine($"Количество чисел в числе {number} равно {countNumber}");
+}
+
+
+int CountNumber(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count += 1;
+    }
+    return count;
+}
