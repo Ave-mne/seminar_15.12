d1acde93a43c1e081a591633c363f8359de4ae38
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-01-17 17:42:33 +0300

    Решена 34 задача

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив заполненый случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = CreateArrayRndInt(10,100,1000);
+PrintArray(array);
+int countElements = CountElements(array);
+Console.WriteLine($"Количество четных чисел в массиве = {countElements}");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max +1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+
+int CountElements(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
