d8d62f055729e5bae00511b3759d3161d62592a7
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-01-20 14:47:02 +0300

    Решена 42 задача

---
+++ Task42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+string res = ConverseDecToBin(13);
+Console.WriteLine(res);
+
+string ConverseDecToBin(int num)
+{
+    string result = string.Empty;
+    while(num > 0)
+    {
+        result =  num % 2 + result; 
+        num /=2;
+    }
+    return result;
+}
