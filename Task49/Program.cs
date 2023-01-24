f5223258d894f9f6834948ec95e8e307758fdf07
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-01-24 14:51:04 +0300

    Решена 49 задача

---
+++ Task49/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. 
+// Найдите элементы у которых оба индекса четные,
+// и замените эти элементы на их квадраты.
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+FindEvenIndex(array2d);
+PrintMatrix(array2d);
+
+void FindEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 ==0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}    
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max +1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5},");
+            else Console.Write($"{matrix[i, j], 5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
